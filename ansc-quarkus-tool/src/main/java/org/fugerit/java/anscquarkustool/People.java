f33a24d35b6a57b6820790e3a0f2ee5365b9e95f
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-07-09 12:50:15 +0200

    inizializzazione progetto
    mvn org.fugerit.java:fj-doc-maven-plugin:8.13.14:init \
    -DgroupId=org.fugerit.java \
    -DartifactId=ansc-quarkus-tool \
    -Dflavour=quarkus-3 \
    -Dextensions=base,freemarker

---
+++ ansc-quarkus-tool/src/main/java/org/fugerit/java/anscquarkustool/People.java
@@ -0,0 +1,20 @@
+// generated from template 'People.ftl' on 2025-07-09T12:51:13.452+02:00
+package org.fugerit.java.anscquarkustool;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+/*
+* Class used to wrap data to be rendered in the document template
+*/
+@Getter
+@AllArgsConstructor
+public class People {
+
+    private String name;
+
+    private String surname;
+
+    private String title;
+
+}
