c02f24b49ff045cc3c34fbbb8b4e1a8756619fa2
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-07-13 17:29:41 +0200

    Rimozione servizi rest di esempio

--- src/test/java/test/org/fugerit/java/anscquarkustool/DocResourceIT.java
+++
@@ -1,8 +0,0 @@
-package test.org.fugerit.java.anscquarkustool;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class DocResourceIT extends DocResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- src/test/java/test/org/fugerit/java/anscquarkustool/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package test.org.fugerit.java.anscquarkustool;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
