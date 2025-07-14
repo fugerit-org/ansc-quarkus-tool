f864763a5ebcaba88aaa55e1222eadd621dc6d31
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-07-14 08:05:45 +0200

    OpenAPI schema

--- src/main/java/org/fugerit/java/anscquarkustool/rest/val/ValResource.java
+++ src/main/java/org/fugerit/java/anscquarkustool/rest/val/ValResource.java
@@ -7,7 +7,10 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.fugerit.java.anscquarkustool.rest.val.tipofile.FacadeValidazioneAllegati;
 import org.fugerit.java.anscquarkustool.rest.val.tipofile.ValidazioneResult;
@@ -28,6 +31,18 @@ public class ValResource {
     @Path("/{ext}")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
+    @APIResponse(responseCode = "200",
+            description = "Validazione avvenuta con esito positivo",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = ValOutput.class)))
+    @APIResponse(responseCode = "400",
+            description = "Validazione avvenuta con esito negativo",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = ValOutput.class)))
+    @APIResponse(responseCode = "500",
+            description = "Validazione avvenuta con errore",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = ValOutput.class)))
     @Operation(
             summary = "Validazione file allegato",
             description = "Valida un file con estensione data tra quelle consentite nel sistema ANSC (PDF, JPG, TIF, P7M)."
@@ -35,7 +50,7 @@ public class ValResource {
     @Tag(name="validazione", description="Validazione file allegati")
     public Response validateFile(@Parameter(description = "Estensione del file (PDF, JPG, TIF, P7M)") @PathParam("ext") String ext, @RestForm InputStream file) {
         ValOutput output = new ValOutput();
-        int status = Response.Status.BAD_REQUEST.getStatusCode();
+        output.setValid(Boolean.FALSE);
         try {
             ValidazioneResult result = this.facade.validate( file );
             log.info( "valid? : {}", result );
@@ -45,7 +60,7 @@ public class ValResource {
                 return Response.ok(output).build();
             }
         } catch (Exception e) {
-            output.setValid(Boolean.FALSE);
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR.getStatusCode() ).entity(output).build();
         }
         return Response.status( Response.Status.BAD_REQUEST.getStatusCode() ).entity(output).build();
     }
